bae6ad2d86bbafa07f2d145af88127514ca895e4
Darren Shepherd <darren.s.shepherd@gmail.com>
2014-04-15 11:04:02 -0700

    Various constants

--- code/iaas/model/src/main/java/io/cattle/platform/core/constants/InstanceConstants.java
+++ code/iaas/model/src/main/java/io/cattle/platform/core/constants/InstanceConstants.java
@@ -2,16 +2,20 @@ package io.cattle.platform.core.constants;
 
 public class InstanceConstants {
 
-    public static final String FIELD_VOLUME_OFFERING_IDS = "volumeOfferingIds";
-    public static final String FIELD_START_ON_CREATE = "startOnCreate";
+    public static final String TYPE = "instance";
+
+    public static final String FIELD_AGENT_INSTANCE = "agentInstance";
+    public static final String FIELD_ENVIRONMENT = "environment";
     public static final String FIELD_IMAGE_UUID = "imageUuid";
     public static final String FIELD_IMAGE_ID = "imageId";
     public static final String FIELD_NETWORK_IDS = "networkIds";
-    public static final String FIELD_VNET_IDS = "vnetIds";
     public static final String FIELD_PORTS = "ports";
     public static final String FIELD_PRIMARY_IP_ADDRESS = "primaryIpAddress";
     public static final String FIELD_REQUESTED_HOST_ID = "requestedHostId";
     public static final String FIELD_SUBNET_IDS = "subnetIds";
+    public static final String FIELD_START_ON_CREATE = "startOnCreate";
+    public static final String FIELD_VNET_IDS = "vnetIds";
+    public static final String FIELD_VOLUME_OFFERING_IDS = "volumeOfferingIds";
 
     public static final String REMOVE_OPTION = "remove";
 
@@ -19,4 +23,6 @@ public class InstanceConstants {
 
     public static final String KIND_CONTAINER = "container";
     public static final String KIND_VIRTUAL_MACHINE = "virtualMachine";
+
+    public static final String STATE_RUNNING = "running";
 }
