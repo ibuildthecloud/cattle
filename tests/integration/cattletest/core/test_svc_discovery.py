71c39beca1ee9f6073c347cdef756a2a4611047a
Darren Shepherd <darren@rancher.com>
2016-03-12 09:11:57 -0700

    Add startOnCreate to service

--- tests/integration/cattletest/core/test_svc_discovery.py
+++ tests/integration/cattletest/core/test_svc_discovery.py
@@ -2814,6 +2814,7 @@ def test_dns_label_and_dns_param(client, context):
     assert c.dnsSearch == ["foo"]
 
 
+<<<<<<< HEAD
 def test_standalone_container_endpoint(new_context):
     client = new_context.client
     host = new_context.host
@@ -2870,3 +2871,19 @@ def test_standalone_container_endpoint(new_context):
 
     assert svc_e is not None
     assert c_e is None
+
+
+def test_service_start_on_create(client, context):
+    env = _create_stack(client)
+
+    image_uuid = context.image_uuid
+    launch_config = {"imageUuid": image_uuid}
+
+    svc = client.create_service(name=random_str(),
+                                environmentId=env.id,
+                                startOnCreate=True,
+                                launchConfig=launch_config)
+    assert svc.startOnCreate
+
+    svc = client.wait_success(svc)
+    assert svc.state == 'active'
